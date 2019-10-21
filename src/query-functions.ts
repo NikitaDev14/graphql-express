45e682ed0711e571d29c2d8dbabfbe43f04ab99b
NikitaDev14 <nick.babych@atiim.com>
2019-10-21 15:37:14 +0300

    Reformatted code.

---
+++ src/query-functions.ts
@@ -0,0 +1,67 @@
+import { Observable } from 'rxjs';
+import { fromFetch } from 'rxjs/fetch';
+import { mergeMap } from 'rxjs/operators';
+
+export const request = (
+  endpoint: string,
+  body: object,
+  extraOptions?: RequestInit,
+): Observable<any> => {
+  const header: Headers = new Headers({
+                                        'Content-Type': 'application/json',
+                                      });
+
+  return fromFetch(
+    endpoint,
+    {
+      ...extraOptions,
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: extraOptions ?
+        {
+          ...extraOptions.headers,
+          ...header,
+        } :
+        header,
+    },
+  )
+    .pipe(
+      mergeMap((rawResponse: Response) => rawResponse.json()),
+    );
+};
+
+export const query = (
+  endpoint: string,
+  queryString: string,
+  variables?: { [key: string]: string},
+  fragments?: string[],
+  extraOptions?: RequestInit,
+): Observable<any> => {
+  return request(
+    endpoint,
+    {
+      query: `query ${queryString} \n
+        ${fragments ? fragments.join('\n') : ''}`,
+      variables,
+    },
+    extraOptions,
+  );
+};
+
+export const mutation = (
+  endpoint: string,
+  mutationString: string,
+  variables?: { [key: string]: string},
+  fragments?: string[],
+  extraOptions?: RequestInit,
+): Observable<any> => {
+  return request(
+    endpoint,
+    {
+      mutation: `mutation ${mutationString} \n
+        ${fragments ? fragments.join('\n') : ''}`,
+      variables,
+    },
+    extraOptions,
+  );
+};
